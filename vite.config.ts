19e813960cd57b014c8029c2426bbe01a31e72c8
Mathieu-Lala <m.lala@viridit.com>
2022-05-15 18:55:03 +0900

    fix: vite url base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: 'https://mathieu-lala.github.io/project2/'
 })
